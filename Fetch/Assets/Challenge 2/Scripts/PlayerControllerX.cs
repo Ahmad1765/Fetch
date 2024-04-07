61f68935c29babcc69fba35bd05c9bd3645dc6c2
Ahmad1765 <flyluckyfire@gmail.com>
2024-04-07 20:31:04 +0500

    Project Files

---
+++ Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 1.0f; // Cooldown time between dog spawns
+    private float lastSpawnTime; // Time when the last dog was spawned
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if enough time has passed since the last spawn
+        if (Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            // On spacebar press, spawn a new dog and update the last spawn time
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastSpawnTime = Time.time;
+            }
+        }
+    }
+}
